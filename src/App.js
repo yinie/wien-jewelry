41e5183e3687d904f425c61c81904baab8571fc3
credible <credible@a0119.attlocal.net>
2018-03-05 20:39:53 -0800

    Add static product card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
-import Home from './pages/home.js'; 
+import ProductList from './pages/home.js'; 
 import DetailPage from './pages/detail.js';
 
 
@@ -10,7 +10,7 @@ import DetailPage from './pages/detail.js';
 const App = () => (
   <Router>
     <div>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={ProductList} />
       <Route path="/detail-page" component={DetailPage} />
     </div>
   </Router>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import fire from '../components/firebase.js';
+import imagefile from '../photo.jpg';
 
-class Home extends React.Component {
+
+
+
+
+
+class ProductCard extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      productItems:[],
+
+    }};
+
+    render(){
+      return(
+        <div className='product-card'>
+          <img src={imagefile}/>
+          <li>
+            <span>Pearl Earring</span>
+            <span>$45</span>
+          </li>
+          <li>The materail:xxx</li>
+        </div>
+
+        )};
+
+}
+
+
+class ProductList extends React.Component {
 
   constructor(props) {
     super(props);
@@ -45,6 +75,7 @@ class Home extends React.Component {
 
          })}
         </ul>
+        <ProductCard/>
         
         <Link to="/detail-page">Details</Link>;
       </div>
@@ -55,4 +86,4 @@ class Home extends React.Component {
 
 
 
-export default Home;
+export default ProductList;
