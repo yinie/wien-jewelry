a99af5ccaac83a7c60e1001fd177efc93d7ed435
credible <credible@a0119.attlocal.net>
2018-03-06 21:20:46 -0800

    product card listing using firebase

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,97 +1,78 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import fire from '../components/firebase.js';
-import imagefile from '../photo.jpg';
 import './home.css'
 
 
-
-
-
-
 class ProductCard extends React.Component{
+
   constructor(props){
     super(props);
     this.state = {
-      
+    }
+  };
 
-    }};
-
-    render(){
-      return(
-        <div className='product-card'>
-          <img className='card-img' src={imagefile}/>
-          <div className='card-description'>
-            <span>Pearl Earring</span> <span>$45</span>
-          </div>
-          <div>The materail:xxx</div>
+  render(){
+    return (
+      <div className='product-card'>
+        <img className='card-img' src={this.props.image}/>
+        <div className='card-description'>
+          <span>{this.props.name}</span> <span>{"$" + this.props.price}</span>
         </div>
-
-        )};
-
+        <div>The materail:xxx</div>
+      </div>
+    );
+  };
 }
 
 
-
-
 class ProductList extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {personState:[]};
+    this.state = {
+      productItems:[]
+    };
   }
 
-   componentDidMount(){
-     const personFirebase = fire.database().ref('persons');
-     personFirebase.on('value', (snapshot) => {
-      let persons = snapshot.val();
-      let newState = [];
-      for (let personNum in persons){
-        newState.push({
-          id : personNum,
-          firstname: persons[personNum].firstname,
-          lastname: persons[personNum].lastname
-        }); 
+  componentDidMount(){
+    const itemsFirebase = fire.database().ref('Items');
+    itemsFirebase.once('value', (snapshot)=>{
+      let Items = snapshot.val();
+      let newItems = [];
+      for(let item in Items){
+        newItems.push({
+          itemId : item,
+          name: Items[item].itemName,
+          price: Items[item].price,
+          material: Items[item].material,
+          category: Items[item].category,
+          image: Items[item].images.image1
+        });
       };
-     this.setState({
-       personState: newState
-     });
-
-     });
-
-   }
-
+      this.setState({
+        productItems: newItems
+      });
+    });
+  }
 
   render() {
     return (
       <div> 
         <div className="nav">Top Nav</div>
         <div className="page-container">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          
+          {
+            this.state.productItems.map((item) => {
+              return(
+                <ProductCard key={item.itemId} name={item.name} price={item.price} image={item.image}/>
+              )
+            })  
+          }  
         </div>
-        <ul>
-         {this.state.personState.map((person)=>{
-                  return(
-                    <li key={person.id}>
-                    <p>First Name: {person.firstname}</p>
-                    <p>Last Name: {person.lastname}</p>
-                    </li>
-                  )
-
-         })}
-        </ul>
-        
-        
         <Link to="/detail-page">Details</Link>;
       </div>
 
     )
   }
 }
-
-
 
 export default ProductList;
