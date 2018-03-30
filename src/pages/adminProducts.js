5b6ab901577da43c3c75133647e5d131d9e0d8ac
credible <credible@a0119.attlocal.net>
2018-03-29 21:39:43 -0700

    uploading image and preivew

--- src/pages/adminProducts.js
+++ src/pages/adminProducts.js
@@ -1,31 +1,61 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from 'react';
 import fire from '../components/firebase.js';
+import './adminProduct.css';
 
 class ImageUpload extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			ifImage: false,
+			imageURL: ''
+		}
 		this.onUpload = this.onUpload.bind(this);
+		
 	}
 
 	onUpload(){
   	const curFiles = this.fileUpload.files;
-  	console.log(curFiles)
+  	const src = window.URL.createObjectURL(curFiles[0]);
+  	const fileName = 'images/' + this.props.name + '.jpg'
   	var storageRef = fire.storage().ref();
-		var mountainImagesRef = storageRef.child('images/mountains.jpg');
-		mountainImagesRef.put(curFiles[0]).then(function(snapshot) {
-  		console.log(snapshot);
+		var fileRef = storageRef.child(fileName);
+		fileRef.put(curFiles[0]).then((snapshot) => {
+			fileRef.getMetadata().then((metadata) =>{
+	  		const imageURL = snapshot.downloadURL
+	  		console.log('url: ' + imageURL)
+	  		this.setState({imageURL:imageURL})
+	  	
+			}).catch(function(error) {
+  		// Uh-oh, an error occurred!
+			});
 		});
+
+		if (curFiles.length === 0){
+			this.setState({ifImage: false})
+		}else{
+			this.setState({
+				ifImage: true,
+			})
+		}		
   }
 
+
+ 
+
 	render(){
+		let preview;
+		if (this.state.imageURL != ''){
+			preview = (<img className="preview-image" alt="image preview" src={this.state.imageURL} />);
+		}else if (this.state.ifImage){
+			preview = (<p>Loading</p>);
+		}
+
 		return(
 			<div>
-				<label for="image_uploads">Choose images to upload (PNG, JPG)</label>
-    		<input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" ref={(ref) => this.fileUpload = ref} onChange={this.onUpload}/>
-    		<div className="preview">
-				  <p>No files currently selected for upload</p>
-				 </div>
+				<label>Choose images to upload (PNG, JPG)
+    			<input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" ref={(ref) => this.fileUpload = ref} onChange={this.onUpload}/>
+    		</label>
+    		<div>{preview}</div>
 			</div>
 		)
 	}
@@ -86,7 +116,7 @@ class ProductForm extends React.Component{
 						<input type="text" name="price" value={price} onChange={this.onChange} />
 					</label>
 
-					<ImageUpload />
+					<ImageUpload  name={itemName}/>
 					
 				  
           <button onClick={this.onSubmit}>Submit</button>
